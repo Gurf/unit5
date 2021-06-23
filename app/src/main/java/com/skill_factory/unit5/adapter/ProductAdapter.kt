f4362b9f4bbab50c077c9c507caab740014f2e86
Gennadii Ustiuzhinskii <mampormin@gmail.com>
2021-06-24 00:33:35 +0300

    whithout delegates

--- app/src/main/java/com/skill_factory/unit5/adapter/ProductAdapter.kt
+++ app/src/main/java/com/skill_factory/unit5/adapter/ProductAdapter.kt
@@ -7,19 +7,54 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.skill_factory.unit5.R
+import com.skill_factory.unit5.model.Add
+import com.skill_factory.unit5.model.Item
 import com.skill_factory.unit5.model.Product
 import java.util.ArrayList
 
-class ProductAdapter(var data: ArrayList<Product>) : RecyclerView.Adapter<ProductAdapter.ViewHolder>() {
+const val ITEM_VIEW_TYPE_PRODUCT = 0
+const val ITEM_VEW_TYPE_ADD = 1
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false))
+ class ProductAdapter(var data: ArrayList<Item>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        if (viewType == ITEM_VIEW_TYPE_PRODUCT)
+            return ViewHolder(
+                LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false)
+            )
+        else if (viewType == ITEM_VEW_TYPE_ADD) {
+            return AddViewHolder(
+                LayoutInflater.from(parent.context).inflate(R.layout.item_add, parent, false)
+            )
+        } else {
+            throw IllegalArgumentException("Invalid viewType")
+        }
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.icon.setImageResource(data[position].idIcon)
-        holder.textName.text = data[position].name
-        holder.textDesc.text = data[position].desc
+    override fun getItemViewType(position: Int): Int {
+        return if (data[position] is Product) {
+            ITEM_VIEW_TYPE_PRODUCT
+        } else if (data[position] is Add) {
+            ITEM_VEW_TYPE_ADD
+        } else {
+            throw IllegalArgumentException("Invalid Item View type")
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (getItemViewType(position) == ITEM_VIEW_TYPE_PRODUCT){
+            val h = holder as ViewHolder
+            val  item = data[position] as Product
+            h.icon.setImageResource(item.idIcon)
+            h.textName.text = item.name
+            h.textDesc.text = item.desc
+        }
+        else if (getItemViewType(position) == ITEM_VEW_TYPE_ADD){
+            val h = holder as AddViewHolder
+            val  item = data[position] as Add
+            h.textTitle.text = item.title
+            h.textContent.text = item.content
+        }
     }
 
 
@@ -31,6 +66,11 @@ class ProductAdapter(var data: ArrayList<Product>) : RecyclerView.Adapter<Produc
         val icon = itemView.findViewById<ImageView>(R.id.icon)
         val textName = itemView.findViewById<TextView>(R.id.text_name)
         val textDesc = itemView.findViewById<TextView>(R.id.text_desc)
+    }
+
+    class AddViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textTitle = itemView.findViewById<TextView>(R.id.title)
+        val textContent = itemView.findViewById<TextView>(R.id.content)
     }
 
 }
---
+++ app/src/main/java/com/skill_factory/unit5/model/Add.kt
@@ -0,0 +1,4 @@
+package com.skill_factory.unit5.model
+
+class Add(val title: String, val content: String) : Item {
+}
